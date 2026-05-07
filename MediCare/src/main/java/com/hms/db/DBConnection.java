a01cdab338640f4713f9e4df6719ee381ddde264
Sriram <162324287+Sriram-DSCE@users.noreply.github.com>
2026-05-07 15:24:55 +0530

    Update database connection details for DBConnection

--- MediCare/src/main/java/com/hms/db/DBConnection.java
+++ MediCare/src/main/java/com/hms/db/DBConnection.java
@@ -20,12 +20,12 @@ public class DBConnection {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			//step:2- create a connection
-			String host = getEnvOrDefault("DB_HOST", "localhost");
+			String host = getEnvOrDefault("DB_HOST", "gfj-ow.h.filess.io");
 			String port = getEnvOrDefault("DB_PORT", "3306");
-			String database = getEnvOrDefault("DB_NAME", "hospitals");
-			String username = getEnvOrDefault("DB_USER", "root");
-			String password = getEnvOrDefault("DB_PASSWORD", "");
-			String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+			String database = getEnvOrDefault("DB_NAME", "hospitals_tailahead");
+			String username = getEnvOrDefault("DB_USER", "hospitals_tailahead");
+			String password = getEnvOrDefault("DB_PASSWORD", "79d8e44786b25049b63dca1c01860199538347fa");
+			String url = "jdbc:mysql://" + gfj-ow.h.filess.io + ":" + 3306 + "/" + hospitals_tailahead;
 			conn = DriverManager.getConnection(url, username, password);
 			
 		} catch (Exception e) {
